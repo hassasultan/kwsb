b4489c4f0304c702c3ed71d0beac4c26ccb3a0fe
Hassan Sultan <h.hsultan277@gmail.com>
2023-03-10 17:01:59 +0500

    hassan

--- resources/views/pages/complaints/index.blade.php
+++ resources/views/pages/complaints/index.blade.php
@@ -19,56 +19,58 @@
         </div>
         <div class="card-body px-0 pb-2">
           <div class="p-0">
-            <table id="example1" class="table table-bordered align-items-center mb-0">
-              <thead>
-                <tr>
-                  <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Town</th>
-                  <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Complaint Type / Priority</th>
-                  <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Title Description</th>
-                  <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Picture</th>
-                  {{-- <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Total Trucks</th> --}}
-                  <th class="text-secondary opacity-7">Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {{-- @if(count($user) > 0) --}}
-                    @foreach ($complaint as $key => $row)
-                        <tr>
-                            <td>
-                                <p class="text-xs font-weight-bold mb-0">{{ $row->town->town }} {{ $row->town->subtown }}</p>
-                            </td>
-                            <td>
-                                <p class="text-xs font-weight-bold mb-0">{{ $row->type?->title }} </p>
-                                <p class="text-xs font-weight-bold mb-0">{{ $row->prio?->title }} </p>
-                            </td>
-                            <td class="align-middle text-center text-sm">
-                                <p class="text-xs text-secondary mb-0">{{ $row->title }}</p>
-                                {{-- <p class="text-xs text-secondary mb-0">{{ $row->description }}</p> --}}
+            <div class="table-responsive">
+                <table id="example1" class="table table-bordered align-items-center mb-0">
+                  <thead>
+                    <tr>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Town</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Complaint Type / Priority</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Title Description</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Picture</th>
+                      {{-- <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Total Trucks</th> --}}
+                      <th class="text-secondary opacity-7">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {{-- @if(count($user) > 0) --}}
+                        @foreach ($complaint as $key => $row)
+                            <tr>
+                                <td>
+                                    <p class="text-xs font-weight-bold mb-0">{{ $row->town->town }} {{ $row->town->subtown }}</p>
+                                </td>
+                                <td>
+                                    <p class="text-xs font-weight-bold mb-0">{{ $row->type?->title }} </p>
+                                    <p class="text-xs font-weight-bold mb-0">{{ $row->prio?->title }} </p>
+                                </td>
+                                <td class="align-middle text-center text-sm">
+                                    <p class="text-xs text-secondary mb-0">{{ $row->title }}</p>
+                                    {{-- <p class="text-xs text-secondary mb-0">{{ $row->description }}</p> --}}
 
-                            </td>
-                            <td class="align-middle text-center text-sm">
-                                @if ($row->image != NULL)
-                                    <img src="{{ asset('public/storage/'.$row->image) }}" class="img-fluid" style="width: 70px; height: 70px;"/>
-                                @else
-                                    Not Available
-                                @endif
-                            </td>
-                            <td class="align-middle">
-                                {{-- <a href="{{ route('compaints-management.edit',$row->id) }}" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
-                                Edit
-                                </a> --}}
-                                <a href="{{ route('compaints-management.details',$row->id) }}" class="text-secondary font-weight-bold text-xs m-3" data-toggle="tooltip" data-original-title="Edit user">
-                                    Assigned
-                                    </a>
+                                </td>
+                                <td class="align-middle text-center text-sm">
+                                    @if ($row->image != NULL)
+                                        <img src="{{ asset('public/storage/'.$row->image) }}" class="img-fluid" style="width: 70px; height: 70px;"/>
+                                    @else
+                                        Not Available
+                                    @endif
+                                </td>
+                                <td class="align-middle">
+                                    {{-- <a href="{{ route('compaints-management.edit',$row->id) }}" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
+                                    Edit
+                                    </a> --}}
+                                    <a href="{{ route('compaints-management.details',$row->id) }}" class="text-secondary font-weight-bold text-xs m-3" data-toggle="tooltip" data-original-title="Edit user">
+                                        Assigned
+                                        </a>
 
-                            </td>
-                        </tr>
-                    @endforeach
-                {{-- @else
-                    No Record Find...
-                @endif --}}
-              </tbody>
-            </table>
+                                </td>
+                            </tr>
+                        @endforeach
+                    {{-- @else
+                        No Record Find...
+                    @endif --}}
+                  </tbody>
+                </table>
+            </div>
           </div>
         </div>
       </div>
