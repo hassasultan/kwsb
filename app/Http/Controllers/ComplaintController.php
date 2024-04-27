dd70eb910949fd16cc608d7d5c0edef213d9bdd4
hassasultan <h.hsultan277@gmail.com>
2024-04-28 02:07:56 +0500

    pushed

--- app/Http/Controllers/ComplaintController.php
+++ app/Http/Controllers/ComplaintController.php
@@ -19,7 +19,7 @@ use App\Traits\SaveImage;
 use Haruncpi\LaravelIdGenerator\IdGenerator;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\DB;
-
+use Exception;
 
 class ComplaintController extends Controller
 {
@@ -38,19 +38,17 @@ class ComplaintController extends Controller
     }
     public function index(Request $request)
     {
-        $complaint = Complaints::with('customer','town','subtown','type','prio','assignedComplaints')->OrderBy('id','DESC');
-        if($request->has('search') && $request->search != null && $request->search != '')
-        {
-            $complaint = $complaint->where('title','LIKE','%'.$request->search.'%')->orWhere('comp_num',$request->search);
+        $complaint = Complaints::with('customer', 'town', 'subtown', 'type', 'prio', 'assignedComplaints')->OrderBy('id', 'DESC');
+        if ($request->has('search') && $request->search != null && $request->search != '') {
+            $complaint = $complaint->where('title', 'LIKE', '%' . $request->search . '%')->orWhere('comp_num', $request->search);
         }
         $complaint = $complaint->paginate(10);
         // dd($complaint->toArray());
-        if($request->has('type'))
-        {
+        if ($request->has('type')) {
             return $complaint;
         }
         // dd($complaint->toArray());
-        return view('pages.complaints.index',compact('complaint'));
+        return view('pages.complaints.index', compact('complaint'));
     }
     public function create(Request $request)
     {
@@ -61,37 +59,31 @@ class ComplaintController extends Controller
         $subtown = SubTown::all();
         $source = Source::all();
         $customer = NULL;
-        if($request->has('search'))
-        {
-            $customer = Customer::where('customer_id',$request->search)->first();
-            if($customer == null)
-            {
+        if ($request->has('search')) {
+            $customer = Customer::where('customer_id', $request->search)->first();
+            if ($customer == null) {
                 return redirect()->back()->with('error', "Customer Not Found...");
             }
         }
 
-        return view('pages.complaints.create',compact('customer','town','type','prio','subtown','subtype','source'));
+        return view('pages.complaints.create', compact('customer', 'town', 'type', 'prio', 'subtown', 'subtype', 'source'));
 
     }
     public function store(Request $request)
     {
         $valid = $this->validator($request->all());
-        if($valid->valid())
-        {
+        if ($valid->valid()) {
             $data = $request->all();
             $prefix = "COMPLAINT-";
-            $CompNum = IdGenerator::generate(['table' => 'complaint','field' => 'comp_num', 'length' => 14, 'prefix' =>$prefix]);
+            $CompNum = IdGenerator::generate(['table' => 'complaint', 'field' => 'comp_num', 'length' => 14, 'prefix' => $prefix]);
             $data['comp_num'] = $CompNum;
-            if($request->has('image') && $request->image != NULL)
-            {
+            if ($request->has('image') && $request->image != NULL) {
                 $data['image'] = $this->complaintImage($request->image);
             }
             Complaints::create($data);
             return redirect()->route('compaints-management.index')->with('success', 'Record created successfully.');
 
-        }
-        else
-        {
+        } else {
             return back()->with('error', $valid->errors());
         }
     }
@@ -101,25 +93,21 @@ class ComplaintController extends Controller
         $town = Town::all();
         $type = ComplaintType::all();
         $subtown = SubTown::all();
-        return view('pages.complaints.edit',compact('complaint','town','type','subtown'));
+        return view('pages.complaints.edit', compact('complaint', 'town', 'type', 'subtown'));
 
     }
-    public function update(Request $request,$id)
+    public function update(Request $request, $id)
     {
         $valid = $this->validator($request->all());
-        if($valid->valid())
-        {
-            $data = $request->except(['_method','_token']);
-            if($request->has('image') && $request->image != NULL)
-            {
+        if ($valid->valid()) {
+            $data = $request->except(['_method', '_token']);
+            if ($request->has('image') && $request->image != NULL) {
                 $data['image'] = $this->complaintImage($request->image);
             }
-            Complaints::where('id',$id)->update($data);
+            Complaints::where('id', $id)->update($data);
             return redirect()->route('compaints-management.index')->with('success', 'Record created successfully.');
 
-        }
-        else
-        {
+        } else {
             return back()->with('error', $valid->errors());
         }
 
@@ -128,26 +116,35 @@ class ComplaintController extends Controller
     {
         $town_id = auth('api')->user()->agent->town_id;
         $type_id = auth('api')->user()->agent->type_id;
-        $complaint = Complaints::with('town','customer','type','subtype','prio')->where('town_id', $town_id)->where('type_id', $type_id)->get();
+        $complaint = Complaints::with('town', 'customer', 'type', 'subtype', 'prio')->where('town_id', $town_id)->where('type_id', $type_id)->get();
         return $complaint;
     }
     public function customer_wise_complaints()
     {
-        $customer_id = auth('api')->user()->customer->id;
-        // $type_id = auth('api')->user()->agent->type_id;
-        $complaint = Complaints::with('town','customer','type','subtype','prio')->where('customer_id', $customer_id)->get();
-        return $complaint;
+        try {
+            $user = auth('api')->user();
+
+            if (!$user || $user->role != 5) {
+                return response()->json(['error' => 'Unauthorized'], 401);
+            }
+            $customer_id = auth('api')->user()->customer->id;
+            // $type_id = auth('api')->user()->agent->type_id;
+            $complaint = Complaints::with('town', 'customer', 'type', 'subtype', 'prio')->where('customer_id', $customer_id)->get();
+            return $complaint;
+        } catch (Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
     public function type_wise_complaints()
     {
         $type_id = auth('api')->user()->agent->type_id;
         $town_id = auth('api')->user()->agent->town_id;
         $typesWithComplaintsCount = ComplaintType::withCount([
-            'complaints' => function (Builder $query) use ($town_id,$type_id) {
+            'complaints' => function (Builder $query) use ($town_id, $type_id) {
                 $query->where('town_id', $town_id)->where('type_id', $type_id);
             }
         ])
-        ->get(['id', 'title']);
+            ->get(['id', 'title']);
         return $typesWithComplaintsCount;
     }
     public function subtype_wise_complaints()
@@ -155,11 +152,11 @@ class ComplaintController extends Controller
         $town_id = auth('api')->user()->agent->town_id;
         $type_id = auth('api')->user()->agent->type_id;
         $subtypesWithComplaintsCount = SubType::withCount([
-            'complaints' => function (Builder $query) use ($town_id,$type_id) {
+            'complaints' => function (Builder $query) use ($town_id, $type_id) {
                 $query->where('town_id', $town_id)->where('type_id', $type_id);
             }
         ])
-        ->get(['id', 'title']);
+            ->get(['id', 'title']);
         return $subtypesWithComplaintsCount;
     }
     public function agent_wise_complaints_count()
@@ -168,60 +165,53 @@ class ComplaintController extends Controller
         $town_id = auth('api')->user()->agent->town_id;
         $type_id = auth('api')->user()->agent->type_id;
 
-        $data['agent'] = MobileAgent::with('assignedComplaints','assignedComplaints.complaints','assignedComplaints.complaints.town')->find(auth('api')->user()->agent->id);
-        $data['total_complaint'] = Complaints::with('town','customer','type','prio')->where('town_id', $town_id)->where('type_id', $type_id)->count();
-        $data['total_complaint_pending'] = Complaints::where('status',0)->where('town_id', $town_id)->where('type_id', $type_id)->count();
-        $data['total_complaint_complete'] = Complaints::where('status',1)->where('town_id', $town_id)->where('type_id', $type_id)->count();
-        $type = ComplaintType::with('complaints')->whereHas('complaints',function($query)use($town_id, $type_id){
+        $data['agent'] = MobileAgent::with('assignedComplaints', 'assignedComplaints.complaints', 'assignedComplaints.complaints.town')->find(auth('api')->user()->agent->id);
+        $data['total_complaint'] = Complaints::with('town', 'customer', 'type', 'prio')->where('town_id', $town_id)->where('type_id', $type_id)->count();
+        $data['total_complaint_pending'] = Complaints::where('status', 0)->where('town_id', $town_id)->where('type_id', $type_id)->count();
+        $data['total_complaint_complete'] = Complaints::where('status', 1)->where('town_id', $town_id)->where('type_id', $type_id)->count();
+        $type = ComplaintType::with('complaints')->whereHas('complaints', function ($query) use ($town_id, $type_id) {
             $query->where('town_id', $town_id)->where('type_id', $type_id);
         })->get();
-        foreach($type as $key => $row)
-        {
-            $result[++$key] = [$row->title, (int)count($row->complaints)];
+        foreach ($type as $key => $row) {
+            $result[++$key] = [$row->title, (int) count($row->complaints)];
         }
         $data['type_count'] = $result;
         return $data;
     }
     public function agent_complaints_update(Request $request)
     {
-        $complaint = Complaints::with('town','customer','type')->find($request->id);
+        $complaint = Complaints::with('town', 'customer', 'type')->find($request->id);
         $complaint->status = $request->status;
-        if($request->has('before_image'))
-        {
+        if ($request->has('before_image')) {
             $complaint->before_image = $this->before($request->before_image);
         }
-        if($request->has('after_image'))
-        {
+        if ($request->has('after_image')) {
             $complaint->after_image = $this->after($request->after_image);
         }
-        if($request->has('agent_description'))
-        {
+        if ($request->has('agent_description')) {
             $complaint->agent_description = $request->agent_description;
         }
         $complaint->save();
-        return response()->json(["message"=>"Your Given Information Addedd Successfully..."]);
+        return response()->json(["message" => "Your Given Information Addedd Successfully..."]);
     }
     public function detail($id)
     {
-        $complaint = Complaints::with('town','town.agents')->find($id);
-        return view('pages.complaints.details',compact('complaint'));
+        $complaint = Complaints::with('town', 'town.agents')->find($id);
+        return view('pages.complaints.details', compact('complaint'));
 
     }
-    public function assign_complaint($agentId,$complaintId)
+    public function assign_complaint($agentId, $complaintId)
     {
-        $check = ComplaintAssignAgent::where('complaint_id',$complaintId)->where('agent_id',$agentId)->first();
-        if($check == null)
-        {
+        $check = ComplaintAssignAgent::where('complaint_id', $complaintId)->where('agent_id', $agentId)->first();
+        if ($check == null) {
             $check = ComplaintAssignAgent::create([
                 'complaint_id' => $complaintId,
                 'agent_id' => $agentId,
             ]);
+        } else {
+            return redirect()->back()->with('error', "Already Assigned this Complaint...!");
         }
-        else
-        {
-            return redirect()->back()->with('error',"Already Assigned this Complaint...!");
-        }
-        return redirect()->route('agent-management.details',$agentId);
+        return redirect()->route('agent-management.details', $agentId);
     }
     public function report()
     {
@@ -229,7 +219,7 @@ class ComplaintController extends Controller
         $type = ComplaintType::get();
         $prio = Priorities::get();
         $source = Complaints::get()->groupBy('source');
-        return view('pages.reports.index',compact('town','type','prio','source'));
+        return view('pages.reports.index', compact('town', 'type', 'prio', 'source'));
 
     }
     public function generate_report(Request $request)
@@ -251,48 +241,42 @@ class ComplaintController extends Controller
         //     ->groupBy([ function ($post) {
         //         return $post->created_at->format('Y-m-d');
         //     },'type_id']);
-        $complaints = Complaints::with('type','customer')
-        ->select('type_id', DB::raw('date(created_at) as date'), DB::raw('count(*) as num_complaints'))
-        ->whereBetween('created_at', [$dateS, $dateE]);
-        if($request->has('town_id'))
-        {
-            $complaints = $complaints->where('town_id',$request->town_id);
+        $complaints = Complaints::with('type', 'customer')
+            ->select('type_id', DB::raw('date(created_at) as date'), DB::raw('count(*) as num_complaints'))
+            ->whereBetween('created_at', [$dateS, $dateE]);
+        if ($request->has('town_id')) {
+            $complaints = $complaints->where('town_id', $request->town_id);
             $town = Town::find($request->town_id);
             // dd($town->toArray());
         }
-        if($request->has('type_id'))
-        {
-            $complaints = $complaints->where('type_id',$request->type_id);
+        if ($request->has('type_id')) {
+            $complaints = $complaints->where('type_id', $request->type_id);
             $type = ComplaintType::find($request->type_id);
             // dd($town->toArray());
         }
-        if($request->has('prio_id'))
-        {
-            $complaints = $complaints->where('prio_id',$request->prio_id);
+        if ($request->has('prio_id')) {
+            $complaints = $complaints->where('prio_id', $request->prio_id);
             $prio = Priorities::find($request->prio_id);
             // dd($town->toArray());
         }
-        if($request->has('customer_id'))
-        {
+        if ($request->has('customer_id')) {
             $cust = $request->customer_id;
-            $complaints = $complaints->WhereHas('customer',function($query)use($cust){
-                $query->where('customer_id',$cust);
-            })->orwhere('customer_num',$request->customer_id);
+            $complaints = $complaints->WhereHas('customer', function ($query) use ($cust) {
+                $query->where('customer_id', $cust);
+            })->orwhere('customer_num', $request->customer_id);
             $consumer = $cust;
             // dd($town->toArray());
         }
-        if($request->has('source'))
-        {
-            if($request->source != "all")
-            {
-                $complaints = $complaints->where('source',$request->source);
+        if ($request->has('source')) {
+            if ($request->source != "all") {
+                $complaints = $complaints->where('source', $request->source);
             }
             $source = $request->source;
             // dd($town->toArray());
         }
         $complaints = $complaints->groupBy('type_id', 'date')
-        ->orderBy('date','ASC')
-        ->get();
+            ->orderBy('date', 'ASC')
+            ->get();
 
         // $type = ComplaintType::get();
         //     ->groupBy([function ($post) {
@@ -301,6 +285,6 @@ class ComplaintController extends Controller
 
         // dd($comp);
         // dd($complaints->toArray());
-        return view('pages.reports.report',compact('complaints','type','dateS','dateE','town','consumer','source','prio'));
+        return view('pages.reports.report', compact('complaints', 'type', 'dateS', 'dateE', 'town', 'consumer', 'source', 'prio'));
     }
 }
