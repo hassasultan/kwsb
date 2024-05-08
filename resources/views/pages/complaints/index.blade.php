aa578c417e1462a88eaab28d290583e1f40e4f88
hassasultan <h.hsultan277@gmail.com>
2024-05-08 15:18:59 +0500

    pushed

--- resources/views/pages/complaints/index.blade.php
+++ resources/views/pages/complaints/index.blade.php
@@ -62,8 +62,8 @@
     <div class="container-fluid">
         <div class="row justify-content-center">
             <div class="col-12">
-                <h2 class="page-title">Comlaint Management</h2>
-                <p> Tables with built-in bootstrap styles </p>
+                <h2 class="page-title">Complaint Management</h2>
+                {{-- <p> Tables with built-in bootstrap styles </p> --}}
                 <div class="col-12 text-right">
                     <a class="btn btn-primary" href="{{ route('compaints-management.create') }}">add</i>&nbsp;&nbsp;<i
                             class="fa fa-user"></i></a>
