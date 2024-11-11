0bd822a2e38d48bb7e96a7c451746048a22e7ae4
hassasultan <96184043+hassasultan@users.noreply.github.com>
2024-11-11 16:20:19 +0500

    PUSHED

--- resources/views/layouts/include/sidebar.blade.php
+++ resources/views/layouts/include/sidebar.blade.php
@@ -5,25 +5,27 @@
     </a>
     <nav class="vertnav navbar navbar-light mx-n3">
         <!-- nav bar -->
-        <div class="w-100 mb-4 d-flex">
-            <a class="navbar-brand mx-auto mt-2 flex-fill text-center" href="{{ URL('admin/home') }}">
-                <span class="avatar avatar-sm mt-2">
-                    <img src="{{ asset('assets/images/unnamed.png') }}" class="avatar-img rounded-circle w-50"/>
-                      {{-- <img src="../dist/assets/avatars/face-1.jpg" alt="..." class="avatar-img rounded-circle"> --}}
-                  </span>
-        {{-- <img src="{{ asset('assets/images/unnamed.png') }}" class="avatar-img rounded-circle w-100"/> --}}
+        @can('home')
+            <div class="w-100 mb-4 d-flex">
+                <a class="navbar-brand mx-auto mt-2 flex-fill text-center" href="{{ URL('admin/home') }}">
+                    <span class="avatar avatar-sm mt-2">
+                        <img src="{{ asset('assets/images/unnamed.png') }}" class="avatar-img rounded-circle w-50" />
+                        {{-- <img src="../dist/assets/avatars/face-1.jpg" alt="..." class="avatar-img rounded-circle"> --}}
+                    </span>
+                    {{-- <img src="{{ asset('assets/images/unnamed.png') }}" class="avatar-img rounded-circle w-100"/> --}}
 
-                {{-- <svg version="1.1" id="logo" class="navbar-brand-img brand-sm" xmlns="http://www.w3.org/2000/svg"
-                    xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 120 120"
-                    xml:space="preserve">
-                    <g>
-                        <polygon class="st0" points="78,105 15,105 24,87 87,87 	" />
-                        <polygon class="st0" points="96,69 33,69 42,51 105,51 	" />
-                        <polygon class="st0" points="78,33 15,33 24,15 87,15 	" />
-                    </g>
-                </svg> --}}
-            </a>
-        </div>
+                    {{-- <svg version="1.1" id="logo" class="navbar-brand-img brand-sm" xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 120 120"
+                        xml:space="preserve">
+                        <g>
+                            <polygon class="st0" points="78,105 15,105 24,87 87,87 	" />
+                            <polygon class="st0" points="96,69 33,69 42,51 105,51 	" />
+                            <polygon class="st0" points="78,33 15,33 24,15 87,15 	" />
+                        </g>
+                    </svg> --}}
+                </a>
+            </div>
+        @endcan
 
         <ul class="navbar-nav flex-fill w-100 mb-2">
             <li class="nav-item dropdown">
@@ -32,14 +34,18 @@
                     <span class="ml-3 item-text">D&R</span><span class="sr-only">(current)</span>
                 </a>
                 <ul class="collapse list-unstyled w-100 show" id="dashboard">
-                    <li class="nav-item @if (Route::is('home')) active @endif">
-                        <a class="nav-link " href="{{ route('home') }}"><span
-                                class="ml-1 item-text">Dashboard</span></a>
-                    </li>
-                    <li class="nav-item @if (Route::is('admin.reports')) active @endif">
-                        <a class="nav-link " href="{{ route('admin.reports') }}"><span
-                                class="ml-1 item-text">Reports</span></a>
-                    </li>
+                    @can('home')
+                        <li class="nav-item @if (Route::is('home')) active @endif">
+                            <a class="nav-link " href="{{ route('home') }}"><span
+                                    class="ml-1 item-text">Dashboard</span></a>
+                        </li>
+                    @endcan
+                    @can('admin.reports')
+                        <li class="nav-item @if (Route::is('admin.reports')) active @endif">
+                            <a class="nav-link " href="{{ route('admin.reports') }}"><span
+                                    class="ml-1 item-text">Reports</span></a>
+                        </li>
+                    @endcan
                 </ul>
             </li>
         </ul>
@@ -53,38 +59,62 @@
                     <span class="ml-3 item-text">Activities</span>
                 </a>
                 <ul class="collapse list-unstyled w-100 show" id="ui-elements">
-                    <li class="nav-item @if (Route::is('announcements.index')) active @endif">
-                        <a class="nav-link" href="{{ url('admin/announcements') }}"><span class="ml-1 item-text">Announcement</span>
-                        </a>
-                    </li>
-                    <li class="nav-item @if (Route::is('user-management.index')) active @endif">
-                        <a class="nav-link" href="{{ url('admin/user-management') }}"><span class="ml-1 item-text">User Management</span>
-                        </a>
-                    </li>
-                    <li class="nav-item @if (Route::is('roles.index')) active @endif">
-                        <a class="nav-link" href="{{ url('admin/roles') }}"><span class="ml-1 item-text">Role Management</span>
-                        </a>
-                    </li>
-                    <li class="nav-item @if (Route::is('permissions.index')) active @endif">
-                        <a class="nav-link" href="{{ url('admin/permissions') }}"><span class="ml-1 item-text">Permissions Management</span>
-                        </a>
-                    </li>
-                    <li class="nav-item @if (Route::is('source-management.index')) active @endif">
-                        <a class="nav-link" href="{{ url('admin/source-management') }}"><span class="ml-1 item-text">Source Management</span>
-                        </a>
-                    </li>
-                    <li class="nav-item @if (Route::is('town-management.index')) active @endif">
-                        <a class="nav-link" href="{{ url('admin/town-management') }}"><span class="ml-1 item-text">Town Management</span>
-                        </a>
-                    </li>
-                    <li class="nav-item @if (Route::is('districts.index')) active @endif">
-                        <a class="nav-link" href="{{ url('admin/districts') }}"><span class="ml-1 item-text">District Management</span>
-                        </a>
-                    </li>
-                    <li class="nav-item @if (Route::is('subtown-management.index')) active @endif">
-                        <a class="nav-link" href="{{ url('admin/subtown-management') }}"><span class="ml-1 item-text">SubTown Management</span>
-                        </a>
-                    </li>
+                    @can('announcements.index')
+                        <li class="nav-item @if (Route::is('announcements.index')) active @endif">
+                            <a class="nav-link" href="{{ url('admin/announcements') }}"><span
+                                    class="ml-1 item-text">Announcement</span>
+                            </a>
+                        </li>
+                    @endcan
+                    @can('user-management.index')
+                        <li class="nav-item @if (Route::is('user-management.index')) active @endif">
+                            <a class="nav-link" href="{{ url('admin/user-management') }}"><span class="ml-1 item-text">User
+                                    Management</span>
+                            </a>
+                        </li>
+                    @endcan
+                    @can('roles.index')
+                        <li class="nav-item @if (Route::is('roles.index')) active @endif">
+                            <a class="nav-link" href="{{ url('admin/roles') }}"><span class="ml-1 item-text">Role
+                                    Management</span>
+                            </a>
+                        </li>
+                    @endcan
+                    @can('permissions.index')
+                        <li class="nav-item @if (Route::is('permissions.index')) active @endif">
+                            <a class="nav-link" href="{{ url('admin/permissions') }}"><span
+                                    class="ml-1 item-text">Permissions Management</span>
+                            </a>
+                        </li>
+                    @endcan
+                    @can('source-management.index')
+                        <li class="nav-item @if (Route::is('source-management.index')) active @endif">
+                            <a class="nav-link" href="{{ url('admin/source-management') }}"><span
+                                    class="ml-1 item-text">Source Management</span>
+                            </a>
+                        </li>
+                    @endcan
+                    @can('town-management.index')
+                        <li class="nav-item @if (Route::is('town-management.index')) active @endif">
+                            <a class="nav-link" href="{{ url('admin/town-management') }}"><span class="ml-1 item-text">Town
+                                    Management</span>
+                            </a>
+                        </li>
+                    @endcan
+                    @can('districts.index')
+                        <li class="nav-item @if (Route::is('districts.index')) active @endif">
+                            <a class="nav-link" href="{{ url('admin/districts') }}"><span class="ml-1 item-text">District
+                                    Management</span>
+                            </a>
+                        </li>
+                    @endcan
+                    @can('subtown-management.index')
+                        <li class="nav-item @if (Route::is('subtown-management.index')) active @endif">
+                            <a class="nav-link" href="{{ url('admin/subtown-management') }}"><span
+                                    class="ml-1 item-text">SubTown Management</span>
+                            </a>
+                        </li>
+                    @endcan
                 </ul>
             </li>
             <li class="nav-item dropdown">
@@ -93,19 +123,30 @@
                     <span class="ml-3 item-text">Agents & Others</span>
                     {{-- <span class="badge badge-pill badge-primary">New</span> --}}
                 </a>
+
                 <ul class="collapse list-unstyled w-100 show" id="forms">
-                    <li class="nav-item @if (Route::is('agent-management.index')) active @endif">
-                        <a class="nav-link" href="{{ url('admin/agent-management') }}"><span class="ml-1 item-text">Agents Management</span>
-                        </a>
-                    </li>
-                    <li class="nav-item @if (Route::is('customer-management.index')) active @endif">
-                        <a class="nav-link" href="{{ url('admin/customer-management') }}"><span class="ml-1 item-text">Customers Management</span>
-                        </a>
-                    </li>
-                    <li class="nav-item @if (Route::is('priorities-management.index')) active @endif">
-                        <a class="nav-link" href="{{ url('admin/priorities-management') }}"><span class="ml-1 item-text">Priorities Management</span>
-                        </a>
-                    </li>
+                    @can('agent-management.index')
+                        <li class="nav-item @if (Route::is('agent-management.index')) active @endif">
+                            <a class="nav-link" href="{{ url('admin/agent-management') }}"><span
+                                    class="ml-1 item-text">Agents Management</span>
+                            </a>
+                        </li>
+                    @endcan
+                    @can('customer-management.index')
+                        <li class="nav-item @if (Route::is('customer-management.index')) active @endif">
+                            <a class="nav-link" href="{{ url('admin/customer-management') }}"><span
+                                    class="ml-1 item-text">Customers Management</span>
+                            </a>
+                        </li>
+                    @endcan
+                    @can('priorities-management.index')
+                        <li class="nav-item @if (Route::is('priorities-management.index')) active @endif">
+                            <a class="nav-link" href="{{ url('admin/priorities-management') }}"><span
+                                    class="ml-1 item-text">Priorities Management</span>
+                            </a>
+                        </li>
+                    @endcan
+
                 </ul>
             </li>
             <li class="nav-item dropdown">
@@ -114,18 +155,27 @@
                     <span class="ml-3 item-text">Complaints Management</span>
                 </a>
                 <ul class="collapse list-unstyled w-100 show" id="tables">
-                    <li class="nav-item @if (Route::is('compaints-management.index')) active @endif">
-                        <a class="nav-link" href="{{ url('admin/compaints-management') }}"><span class="ml-1 item-text">Complaints</span>
-                        </a>
-                    </li>
-                    <li class="nav-item @if (Route::is('compaints-type-management.index')) active @endif">
-                        <a class="nav-link" href="{{ url('admin/compaints-type-management') }}"><span class="ml-1 item-text">CT Management</span>
-                        </a>
-                    </li>
-                    <li class="nav-item @if (Route::is('subtype-management.index')) active @endif">
-                        <a class="nav-link" href="{{ url('admin/subtype-management') }}"><span class="ml-1 item-text">SubType</span>
-                        </a>
-                    </li>
+                    @can('compaints-management.index')
+                        <li class="nav-item @if (Route::is('compaints-management.index')) active @endif">
+                            <a class="nav-link" href="{{ url('admin/compaints-management') }}"><span
+                                    class="ml-1 item-text">Complaints</span>
+                            </a>
+                        </li>
+                    @endcan
+                    @can('compaints-type-management.index')
+                        <li class="nav-item @if (Route::is('compaints-type-management.index')) active @endif">
+                            <a class="nav-link" href="{{ url('admin/compaints-type-management') }}"><span
+                                    class="ml-1 item-text">CT Management</span>
+                            </a>
+                        </li>
+                    @endcan
+                    @can('subtype-management.index')
+                        <li class="nav-item @if (Route::is('subtype-management.index')) active @endif">
+                            <a class="nav-link" href="{{ url('admin/subtype-management') }}"><span
+                                    class="ml-1 item-text">SubType</span>
+                            </a>
+                        </li>
+                    @endcan
                 </ul>
             </li>
         </ul>
@@ -134,4 +184,3 @@
 
 
 {{-- new Sidebar End --}}
-
