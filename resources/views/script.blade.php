d9bfc338a7c8759772a43a2d77e24eec4474574d
hassasultan <h.hsultan277@gmail.com>
2025-04-28 20:30:34 +0500

    pushed

--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -1,27 +1,20 @@
 <script>
     function validateInput(element) {
-        // Only allow letters, numbers, and spaces
+        let maxLength = 0;
+
+        if (element.id === 'customer-name') {
+            maxLength = 25;
+        }
+        if (element.id === 'description-box') {
+            maxLength = 350;
+        }
+
+        // Allow only letters, numbers, and spaces
         element.value = element.value.replace(/[^a-zA-Z0-9 ]/g, '');
 
-        // Enforce 350 characters maximum even after removing special characters
-        if (element.value.length > 350) {
-            element.value = element.value.substring(0, 350);
+        // Limit to maximum allowed characters
+        if (element.value.length > maxLength) {
+            element.value = element.value.substring(0, maxLength);
         }
-    }
-    let maxLength = 0;
-
-    if (element.id === 'customer-name') {
-        maxLength = 25;
-    }
-    if (element.id === 'description-box') {
-        maxLength = 350;
-    }
-
-    // Allow only letters, numbers, and spaces
-    element.value = element.value.replace(/[^a-zA-Z0-9 ]/g, '');
-
-    // Limit to maximum allowed characters
-    if (element.value.length > maxLength) {
-        element.value = element.value.substring(0, maxLength);
     }
 </script>
